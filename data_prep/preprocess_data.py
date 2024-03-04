0357fbac3969ca0e77e559177c6f02ab164b062c
liam mall <29150314+liamcmall@users.noreply.github.com>
2024-03-04 16:40:20 +0000

    upload to python via script

---
+++ data_prep/preprocess_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import boto3
+import os
+
+def clean_data(file_path):
+    df = pd.read_csv(file_path)
+    df_cleaned = df.dropna()
+    return df_cleaned
+
+def upload_to_s3(cleaned_df, bucket_name, object_name):
+    s3_client = boto3.client('s3')
+    cleaned_df.to_csv('cleaned_data.csv', index=False)
+    s3_client.upload_file('cleaned_data.csv', bucket_name, object_name)
+    os.remove('cleaned_data.csv')
+
+if __name__ == "__main__":
+    df_cleaned = clean_data('~/User/Liam/Downloads/cleaned.csv')
+    upload_to_s3(df_cleaned, 'liam-vibration-analysis', 's3://liam-vibration-analysis/data/cleaned_data.csv')
