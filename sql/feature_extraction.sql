0292768585895b6931c0df41ce50ffb9b2734e3a
liam mall <29150314+liamcmall@users.noreply.github.com>
2024-03-04 16:51:04 +0000

    initial sql upload

--- sql/feature_extraction.sql
+++ sql/feature_extraction.sql
@@ -1,6 +1,20 @@
+UNLOAD ('
 SELECT
-    sensor_id,
-    AVG(vibration_value) OVER (PARTITION BY sensor_id ORDER BY timestamp RANGE BETWEEN INTERVAL '1 hour' PRECEDING AND CURRENT ROW) AS avg_vibration_1hr,
-    STDDEV(vibration_value) OVER (PARTITION BY sensor_id ORDER BY timestamp RANGE BETWEEN INTERVAL '1 hour' PRECEDING AND CURRENT ROW) AS stddev_vibration_1hr,
-    COUNT(vibration_value) OVER (PARTITION BY sensor_id ORDER BY timestamp RANGE BETWEEN INTERVAL '1 hour' PRECEDING AND CURRENT ROW) AS count_vibration_1hr
-FROM sensor_data;
+    timestamp,
+    AVG(V_in) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS avg_V_in_1hr,
+    AVG(Measured_RPM) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS avg_RPM_1hr,
+    AVG(Vibration_1) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS avg_Vibration_1_1hr,
+    AVG(Vibration_2) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS avg_Vibration_2_1hr,
+    AVG(Vibration_3) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS avg_Vibration_3_1hr,
+    MAX(Vibration_1) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS max_Vibration_1_1hr,
+    MAX(Vibration_2) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS max_Vibration_2_1hr,
+    MAX(Vibration_3) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS max_Vibration_3_1hr,
+    MIN(Vibration_1) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS min_Vibration_1_1hr,
+    MIN(Vibration_2) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS min_Vibration_2_1hr,
+    MIN(Vibration_3) OVER (PARTITION BY timestamp ORDER BY timestamp RANGE BETWEEN INTERVAL ''1 hour'' PRECEDING AND CURRENT ROW) AS min_Vibration_3_1hr
+FROM
+    sensor_data_table
+')
+TO 's3://liam-vibration-analysis/processed_data/features/'
+/* need IAM ROLE */
+FORMAT AS PARQUET;
